7e9d605cba7493cebbd16cb0336ae269518242a6
JliusoO <Jlius@ya.ru>
2023-03-08 07:42:38 +0300

    fix problem

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -72,8 +72,8 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-        'HOST': os.getenv('DB_HOST', default='postgres'),
-        'PORT': os.getenv('DB_PORT', default='15432')
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
 
