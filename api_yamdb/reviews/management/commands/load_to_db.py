36d489e9c4d78a9b0084b2caa75e47b44805e1d0
JliusoO <Jlius@ya.ru>
2023-03-09 05:43:54 +0300

    fix problem

--- api_yamdb/reviews/management/commands/load_to_db.py
+++ api_yamdb/reviews/management/commands/load_to_db.py
@@ -4,49 +4,26 @@ from django.core.management import BaseCommand
 from reviews.models import Category, Comment, Genre, Review, Title, User
 
 
-# ModToCSV = {
-#     User: "users.csv",
-#     Category: "category.csv",
-#     Genre: "genre.csv",
-#     Title: "titles.csv",
-#     Review: "review.csv",
-#     Comment: "comments.csv",
-# }
-#
-#
-# class Command(BaseCommand):
-#     def handle(self, *args, **options):
-#         for model, csv_file in ModToCSV.items():
-#             with open(
-#                 f"{settings.BASE_DIR}/static/data/{csv_file}",
-#                 "r", encoding="utf-8"
-#             ) as f:
-#                 dict_reader = csv.DictReader(f)
-#                 model.objects.bulk_create(
-#                     model(**data) for data in dict_reader
-#                 )
-#
-#         self.stdout.write(self.style.SUCCESS("Данные успешно загружены"))
-TABLES = {
-    User: 'users.csv',
-    Category: 'category.csv',
-    Genre: 'genre.csv',
-    Title: 'titles.csv',
-    Review: 'review.csv',
-    Comment: 'comments.csv',
+ModToCSV = {
+    User: "users.csv",
+    Category: "category.csv",
+    Genre: "genre.csv",
+    Title: "titles.csv",
+    Review: "review.csv",
+    Comment: "comments.csv",
 }
 
 
 class Command(BaseCommand):
-
-    def handle(self, *args, **kwargs):
-        for model, csv_f in TABLES.items():
+    def handle(self, *args, **options):
+        for model, csv_file in ModToCSV.items():
             with open(
-                f'{settings.BASE_DIR}/static/data/{csv_f}',
-                'r',
-                encoding='utf-8'
-            ) as csv_file:
-                reader = csv.DictReader(csv_file)
+                f"{settings.BASE_DIR}/static/data/{csv_file}",
+                "r", encoding="utf-8"
+            ) as f:
+                dict_reader = csv.DictReader(f)
                 model.objects.bulk_create(
-                    model(**data) for data in reader)
-        self.stdout.write(self.style.SUCCESS('Все данные загружены'))
+                    model(**data) for data in dict_reader
+                )
+
+        self.stdout.write(self.style.SUCCESS("Данные успешно загружены"))
